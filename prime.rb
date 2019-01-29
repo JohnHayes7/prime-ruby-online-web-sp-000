5509bbbab06cb97cd59ec20c21a564cfb11b3917
John Hayes <johnnyhayes7@gmail.com>
2019-01-29 15:05:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+require "pry"
+
+def prime?(number)
+  if number == 2 || number == 3
+    return true
+    elsif number > 3 
+    #square_root = Math.sqrt(number)
+    max_test_int = number - 1
+    test_range_array = (2..max_test_int).to_a
+      test_range_array.all? do |i|
+        number % i != 0
+        ##if number % i == 0
+        ##  return false
+        # else
+        #   return true
+        # end
+    end
+   else
+    return false
+  end
+end
+
